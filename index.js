10c82e5e494981c4df685f0bcb062bc0515c15ee
ClaeLu <clarajlu@gmail.com>
2023-07-18 00:41:32 -0400

    write inner and outer functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(summertimeActivity = "roller-skate") {
+    return `This Saturday, I want to ${summertimeActivity}!`;
+}
+
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function (adj = "special"){
+        return `You are ${visualFlair}${adj}${visualFlair}!`
+    }
+}
+
+
+wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+  
